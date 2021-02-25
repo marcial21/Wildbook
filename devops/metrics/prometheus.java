1058d18d6bebf284d0b16f25c1b3513c516461f4
Joanna Hoang <hoangj21@up.edu>
2021-02-24 16:59:11 -0800

    Fixed spelling error

--- devops/metrics/prometheus.java
+++ devops/metrics/prometheus.java
@@ -32,48 +32,49 @@ public class flukebook
         .name("numSpecies").help("Number of species in Flukebook.").register();
     static final Gauge users = Gauge.build()
         .name("numberOfUsers").help("Number of users in Flukebook.").register();
-    static final Guage usersWLoginP = Gauge.build()
+    static final Gauge usersWLoginP = Gauge.build()
         .name("numberOfUsersWLoginP").help("Number of users with login privileges in Flukebook").register(); 
-    static final Guage usersWOLoginP = Gauge.build()
+    static final Gauge usersWOLoginP = Gauge.build()
         .name("numberOfUsersWOLoginP").help("Number of users with out login privileges in Flukebook").register(); 
-    static final Guage usersActive = Gauge.build()
+    static final Gauge usersActive = Gauge.build()
         .name("numberofUsersActive").help("Number of active users.").register(); 
 
     //Encounters
-    static final Guage encounters = Gauge.build()
+    static final Gauge encounters = Gauge.build()
         .name("numEncounters").help("Number of encounters.").register();
-    static final Guage encountersInAgg = Gauge.build()
+    static final Gauge encountersInAgg = Gauge.build()
         .name("numEncountersInAgg").help("Number of encounters by species.").register();
-    static final Guage encountersByWildbook = Gauge.build()
+    static final Gauge encountersByWildbook = Gauge.build()
         .name("numEncountersByWildbook").help("Number of encounters by Wildbook.").register();
-    static final Guage encountersBySpecies = Gauge.build()
+    static final Gauge encountersBySpecies = Gauge.build()
         .name("numEncountersBySpecies").help("Number of encounters by species.").register();
-    static final Guage encountersBySubmissionDate = Gauge.build()
+    static final Gauge encountersBySubmissionDate = Gauge.build()
         .name("numEncountersBySubmissionDate").help("Number of encounters by submission date.").register();                            
-    static final Guage encountersByData = Gauge.build()
+    static final Gauge encountersByData = Gauge.build()
         .name("numEncountersByData").help("Number of encounters by encounter data.").register();
-    static final Guage encountersByLocationID = Gauge.build()
+    static final Gauge encountersByLocationID = Gauge.build()
         .name("numEncountersByLocationID").help("Number of encounters by location ID.").register();
-    static final Guage encountersGPSCoords = Gauge.build()
+    static final Gauge encountersGPSCoords = Gauge.build()
         .name("encountersGPSCoords").help("Encounters' GPS coordinates.").register();
-    static final Guage encounterGPSCoordsInAgg = Gauge.build()
+    static final Gauge encounterGPSCoordsInAgg = Gauge.build()
         .name("encountersGPSCoordsInAgg").help("Encounters' GPS coordinates in aggregate.").register();
-    static final Guage encountersGPSCoordsBySpecies = Gauge.build()
+    static final Gauge encountersGPSCoordsBySpecies = Gauge.build()
         .name("encountersGPSCoordsBySpecies").help("Encounters' GPS coordinates by species.").register(); 
 
     //Individuals 
-    static final Guage individuals = Gauge.build()
+    static final Gauge individuals = Gauge.build()
         .name("numIndividuals").help("Number of individuals.").register();    
-    static final Guage individualsInAgg = Gauge.build()
+    static final Gauge individualsInAgg = Gauge.build()
         .name("numIndividualsInAgg").help("Number of individuals in aggregate.").register();
-    static final Guage individualsByWildbook = Gauge.build()
+    static final Gauge individualsByWildbook = Gauge.build()
         .name("numIndividualsByWildbook").help("Number of individuals by Wildbook.").register();
-    static final Guage individualsBySpecies = Gauge.build()
+    static final Gauge individualsBySpecies = Gauge.build()
         .name("numIndividualsBySpecies").help("Number of individuals by species.").register();
-    static final Guage individualsMaxYearsReSights = Gauge.build()
+    static final Gauge individualsMaxYearsReSights = Gauge.build()
         .name("numIndividualsMaxYearsReSights").help("Number of individuals with max years between re-sights.").register();
-    static final Guage individualsWithReSight = Gauge.build()
+    static final Gauge individualsWithReSight = Gauge.build()
         .name("numIndividualsWithReSight").help("Number of individuals with at least one re-sight.").register(); 
+        
            
 }
 
